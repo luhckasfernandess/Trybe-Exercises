cfdc652db3fa3d3979f70109369b9f7f52ba1851
Lucas Fernandes <luhckaspessoal@gmail.com>
2022-02-02 18:24:39 -0400

    Basic Arithmetic Operations Exercise Added!

---
+++ modulo-01-fundamentos/bloco-04-Introducao-a-JavaScript-e-Logica-de-Programacao/Exercise-of-the-Day/Basic-Arithmetic-Operation.js
@@ -0,0 +1,8 @@
+/* Seu programa deve ter duas constantes, a e b ,
+definidas no começo com os valores que serão operados */
+const a = 5, b = 5;
+console.log("Adição:",a,"+",b,"=",a+b);
+console.log("Subtração:",a,"-",b,"=",a-b);
+console.log("Multiplicação:",a,"*",b,"=",a*b);
+console.log("Divisão:",a,"/",b,"=",a/b);
+console.log("Módulo:",a,"%",b,"=",a%b);

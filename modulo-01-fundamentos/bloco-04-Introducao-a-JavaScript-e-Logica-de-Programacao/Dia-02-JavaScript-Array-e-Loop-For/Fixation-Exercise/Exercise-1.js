e44f34576fb59016dbf86e9be0f921fd3e5ed289
Lucas Fernandes <luhckaspessoal@gmail.com>
2022-02-03 14:55:45 -0400

    Added Fixation Exercise 1

---
+++ modulo-01-fundamentos/bloco-04-Introducao-a-JavaScript-e-Logica-de-Programacao/Dia-02-JavaScript-Array-e-Loop-For/Fixation-Exercise/Exercise-1.js
@@ -0,0 +1,8 @@
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+
+// Mas e se eu ñ soubesse qual é o nº do índice de 'Serviços' do menu?
+
+let menuServices = menu[menu.indexOf('Serviços')];
+// menuServices = menu[1]; ;)
+
+console.log(menuServices);

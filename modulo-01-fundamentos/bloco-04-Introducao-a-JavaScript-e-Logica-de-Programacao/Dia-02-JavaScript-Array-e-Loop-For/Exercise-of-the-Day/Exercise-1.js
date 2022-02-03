0bcf88f017053386abf1eb88a0aa7ad9c148009c
Lucas Fernandes <luhckaspessoal@gmail.com>
2022-02-03 17:31:53 -0400

    Traversing the array

--- modulo-01-fundamentos/bloco-04-Introducao-a-JavaScript-e-Logica-de-Programacao/Dia-02-JavaScript-Array-e-Loop-For/Exercise-of-the-Day/Exercise-1.js
+++ modulo-01-fundamentos/bloco-04-Introducao-a-JavaScript-e-Logica-de-Programacao/Dia-02-JavaScript-Array-e-Loop-For/Exercise-of-the-Day/Exercise-1.js
@@ -1,15 +1,11 @@
-/*
-Leia atentamente os enunciados e faça o que se pede!
-Iremos utilizar esse array para realizar os exercícios do 1 ao 7
-*/
-
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-
 /* 
 Nesse primeiro exercício, percorra o array imprimindo todos os
 valores nele contidos com a função console.log()
 */
 
-for (let index of numbers) {
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+console.log("Array:\n")
+for (let index of numbers) { // Gostei do For/of
     console.log(index);
 }

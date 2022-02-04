bd8115b893359266b027d373e2e5ca8321a3498e
Lucas Fernandes <luhckaspessoal@gmail.com>
2022-02-04 15:22:51 -0400

    Finished, now let's goto the next day

---
+++ modulo-01-fundamentos/bloco-04-Introducao-a-JavaScript-e-Logica-de-Programacao/Dia-02-JavaScript-Array-e-Loop-For/Exercise-of-the-Day/Bonus.js
@@ -0,0 +1,18 @@
+// Ordene o array numbers em ordem crescente e imprima seus valores
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+for (let index = 1; index < numbers.length; index += 1){ 
+// Marca um valor para começar a comparar
+    for (index2 = 0; index2 < index; index2 += 1){
+    // Compara os valores desde o inínio do array até o valor marcado
+        if (numbers[index] < numbers[index2]){
+        // Verifica os valores menores
+            let position = numbers[index2];
+            numbers[index2] = numbers[index];
+            numbers[index] = position;
+            // Muda as posições
+        }
+    }
+}
+console.log(numbers.length);

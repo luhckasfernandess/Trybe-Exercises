a14ea148bc25341d72f1d87f5b33ecc832775152
Lucas Fernandes <luhckaspessoal@gmail.com>
2022-02-09 19:02:59 -0400

    Change Color Yellow

--- modulo-01-fundamentos/bloco-05-JavaScript-DOM-Eventos-Web-Storage/Dia-01-JavaScript-DOM-e-Seletores/Exercise-of-the-Day/script.js
+++ modulo-01-fundamentos/bloco-05-JavaScript-DOM-Eventos-Web-Storage/Dia-01-JavaScript-DOM-e-Seletores/Exercise-of-the-Day/script.js
@@ -12,7 +12,6 @@ daqui a 2 anos. (Não gaste tempo pensando no texto e sim realizando o exercíci
 
 function changeText(tag, text){
     let newText = document.getElementsByTagName(tag);
-    // console.log(text);
     // Percorrer essa lista de elementos e mudar a cor de cada um
     for (let index = 0; index < newText.length; index += 1) {
       newText[index].innerText = text;
@@ -21,8 +20,16 @@ function changeText(tag, text){
 
  changeText('p', 'Eu me vejo fazendo mais de R$ 3.500 por mês ha ha')
 
+// Crie uma função que mude a cor do quadrado amarelo para o verde da Trybe (rgb(76,164,109))
+
+function changeColor (a, b) {
+    let newColor = document.getElementsByClassName(a);
+    newColor[0].style.background = b;
+}
+
+changeColor("main-content", "rgb(76,164,109)")
+
 /* 
- Crie uma função que mude a cor do quadrado amarelo para o verde da Trybe (rgb(76,164,109)).
  Crie uma função que mude a cor do quadrado vermelho para branco.
  Crie uma função que corrija o texto da tag <h1>.
  Crie uma função que modifique todo o texto da tag <p> para maiúsculo.

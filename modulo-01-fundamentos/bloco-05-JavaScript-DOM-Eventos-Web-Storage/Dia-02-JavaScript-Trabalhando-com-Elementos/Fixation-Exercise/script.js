99705f086f74e17fabfb192884a92e00e5719168
Lucas Fernandes <luhckaspessoal@gmail.com>
2022-02-10 17:22:07 -0400

    Starting the Day's Exercise

---
+++ modulo-01-fundamentos/bloco-05-JavaScript-DOM-Eventos-Web-Storage/Dia-02-JavaScript-Trabalhando-com-Elementos/Fixation-Exercise/script.js
@@ -0,0 +1,14 @@
+// Acesse o elemento elementoOndeVoceEsta
+console.log(document.getElementById("elementoOndeVoceEsta"));
+// Acesse pai a partir de elementoOndeVoceEsta e adicione uma color a ele.
+console.log(document.getElementById("elementoOndeVoceEsta").parentNode = color);
+// Acesse o primeiroFilhoDoFilho e adicione um texto a ele. Você se lembra dos vídeos da aula anterior, como fazer isso?
+console.log();
+
+/* 
+Acesse o primeiroFilho a partir de pai .
+Agora acesse o primeiroFilho a partir de elementoOndeVoceEsta .
+Agora acesse o texto Atenção! a partir de elementoOndeVoceEsta .
+Agora acesse o terceiroFilho a partir de elementoOndeVoceEsta .
+Agora acesse o terceiroFilho a partir de pai .
+*/

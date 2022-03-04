e374e05b2f581fab6159d894a68859c3665e1725
Lucas Fernandes <luhckaspessoal@gmail.com>
2022-03-03 21:18:52 -0400

    Function that returns the longest word

--- modulo-01-fundamentos/bloco-07-Introducao-a-JavaScript-ES6-e-Testes-Unitarios/Exercises-of-the-day/Parte-II/script2.js
+++ modulo-01-fundamentos/bloco-07-Introducao-a-JavaScript-ES6-e-Testes-Unitarios/Exercises-of-the-day/Parte-II/script2.js
@@ -1,6 +1,15 @@
-const longestWord = phrase => {
-   phrase.split(' ').length;
-   //Math.max(larger);
-}
+/* Source: https://pt.stackoverflow.com/questions/426836/como-pegar-a-maior-palavra-de-uma-string-em-javascript */
 
+function longestWord(phrase) {
+    const arrayStr = phrase.split(' '); // Dividindo a frase utilizando o split e salvando na variavel arrayStr
+    let longest = 0;
+    let word = null;
+    arrayStr.forEach(function(str) { // .forEach vai percorrer o array
+            if (longest < str.length) { // Verifica a palavra mais longa
+            longest = str.length; // Guarda o tamanho da palavra mais longa (nº inteiro)
+            word = str; // Atribui/salva a palavra mais longa
+        }
+    });
+    return word; // Retorna o mais longo
+}
 console.log(longestWord('Antônio foi no banheiro e não sabemos o que aconteceu'));

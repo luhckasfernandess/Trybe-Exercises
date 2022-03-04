6fe55ed5a0200779b85641e75e5e158b88918993
Lucas Fernandes <luhckaspessoal@gmail.com>
2022-03-03 20:26:51 -0400

    Trying Part-II.2

--- modulo-01-fundamentos/bloco-07-Introducao-a-JavaScript-ES6-e-Testes-Unitarios/Exercises-of-the-day/Parte-I/script.js
+++ modulo-01-fundamentos/bloco-07-Introducao-a-JavaScript-ES6-e-Testes-Unitarios/Exercises-of-the-day/Parte-I/script.js
@@ -1,3 +1,3 @@
-const testingScope = (escopo) => escopo === true ? console.log('Não devo ser utilizada fora do meu escopo (if) Ótimo, fui utilizada no escopo!') : console.log('Não devo ser utilizada fora meu escopo (else)')
+const testingScope = escopo => escopo === true ? console.log('Não devo ser utilizada fora do meu escopo (if) Ótimo, fui utilizada no escopo!') : console.log('Não devo ser utilizada fora meu escopo (else)')
 
 testingScope(true);
---
+++ modulo-01-fundamentos/bloco-07-Introducao-a-JavaScript-ES6-e-Testes-Unitarios/Exercises-of-the-day/Parte-II/script2.js
@@ -0,0 +1,6 @@
+const longestWord = phrase => {
+   phrase.split(' ').length;
+   //Math.max(larger);
+}
+
+console.log(longestWord('Antônio foi no banheiro e não sabemos o que aconteceu'));

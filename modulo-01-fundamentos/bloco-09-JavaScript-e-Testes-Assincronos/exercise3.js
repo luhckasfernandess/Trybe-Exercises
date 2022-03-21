97c58572d4df7e803aaec158f5be256ae74353d3
Lucas Fernandes <luhckaspessoal@gmail.com>
2022-03-21 18:15:53 -0400

    sendMarsTemperature function with delay of up to 5 seconds

--- modulo-01-fundamentos/bloco-09-JavaScript-e-Testes-Assincronos/exercise3.js
+++ modulo-01-fundamentos/bloco-09-JavaScript-e-Testes-Assincronos/exercise3.js
@@ -1,5 +1,7 @@
-// A função getPlanet abaixo imprime o planeta Marte de forma síncrona.
-// Modifique getPlanet , de forma que Marte seja impresso assincronamente, depois de 4 segundos.
+/*
+ A função getPlanet abaixo imprime o planeta Marte de forma síncrona.
+ Modifique getPlanet , de forma que Marte seja impresso assincronamente, depois de 4 segundos.
+ */
 
 const getPlanet = () => {
     const mars = {

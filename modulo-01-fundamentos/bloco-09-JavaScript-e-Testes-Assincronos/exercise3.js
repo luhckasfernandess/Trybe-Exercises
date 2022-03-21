b4981ccb540e058590044aa36589582eabf06e56
Lucas Fernandes <luhckaspessoal@gmail.com>
2022-03-21 17:50:05 -0400

    Print Mars after 4 seconds

---
+++ modulo-01-fundamentos/bloco-09-JavaScript-e-Testes-Assincronos/exercise3.js
@@ -0,0 +1,19 @@
+// A função getPlanet abaixo imprime o planeta Marte de forma síncrona.
+// Modifique getPlanet , de forma que Marte seja impresso assincronamente, depois de 4 segundos.
+
+const getPlanet = () => {
+    const mars = {
+      name: "Mars",
+      distanceFromSun: {
+        value: 227900000,
+        measurementUnit: "kilometers",
+      },
+    };
+    console.log("Returned planet: ", mars);
+  };
+  
+  setTimeout(() => console.log('1 second'), 1000);
+  setTimeout(() => console.log('2 seconds'), 2000);
+  setTimeout(() => console.log('3 seconds'), 3000);
+  
+  setTimeout(() => getPlanet(), 4000); // imprime Marte depois de 4 segundos

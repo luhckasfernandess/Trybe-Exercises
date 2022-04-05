1f9627cdb75d55f4e8bfbbc78b9d649843319fe1
Lucas Fernandes <luhckaspessoal@gmail.com>
2022-04-05 13:34:20 -0400

    Exercise 3 complete! New react app created

---
+++ modulo-01-fundamentos/bloco-10-Introducao-a-React/Day-01-Hello-World-no-React/new-project/src/Footer.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Footer extends Component {
+    render () {
+        return (
+            <div>
+                <h1>E isso é só o começo...</h1>
+            </div>
+        );
+    }
+}
+
+export default Footer
